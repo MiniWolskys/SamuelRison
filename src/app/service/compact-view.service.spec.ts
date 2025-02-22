155f35fd084dbb57e97444cc4130e57da36b1773
MiniWolskys <samuelrison@yahoo.fr>
2025-02-22 02:29:20 +0100

    Working jest environment

--- src/app/service/compact-view.service.spec.ts
+++ src/app/service/compact-view.service.spec.ts
@@ -3,14 +3,20 @@ import { TestBed } from '@angular/core/testing';
 import { CompactViewService } from './compact-view.service';
 
 describe('CompactViewService', () => {
-  let service: CompactViewService;
+    let service: CompactViewService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CompactViewService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CompactViewService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should have initial value of true', () => {
+        service.isCompactObservable().subscribe(value => {
+            expect(value).toEqual(true);
+        });
+    });
 });
